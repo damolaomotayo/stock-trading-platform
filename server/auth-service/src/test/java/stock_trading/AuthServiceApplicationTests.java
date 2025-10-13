889f4ac25d167b1247a13552d8cf986d51cac531
damola omotayo <omotayodamola91@gmail.com>
2025-10-13 15:59:45 +0100

    feat: initialize a spring boot auth-service

---
+++ server/auth-service/src/test/java/stock_trading/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package stock_trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
