5c19631ed9071caf1cb4249a6dac5069a1c0d012
damola omotayo <omotayodamola91@gmail.com>
2025-10-15 20:10:50 +0100

    feat: user service implementation

---
+++ server/auth-service/src/main/java/stock_trading/service/CustomUserDetailService.java
@@ -0,0 +1,25 @@
+package stock_trading.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import stock_trading.entity.User;
+import stock_trading.entity.UserPrincipal;
+import stock_trading.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email:" + email));
+
+                return UserPrincipal.create(user);
+    }
+}
